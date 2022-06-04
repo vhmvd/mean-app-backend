b03ff3da3d6ddd5f5bbbdb425c4c6f926be5a3b0
Ahmed Nadeem <ahmn21@hotmail.com>
2022-06-04 19:33:40 +0500

    Index

--- server/helper/authenticate.js
+++ server/helper/authenticate.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-function authenticateToken(req,res,next){
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if(token == null) return res.sendStatus(401);
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
 
-    const {SECRET}  = process.env;
-    jwt.verify(token, SECRET , (err,user) => {
-        if(err) return res.sendStatus(403);
-        console.log("authorized")
-        req.user = user;
-        next();
-    })
+  const { SECRET } = process.env;
+  jwt.verify(token, SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    console.log("authorized")
+    req.user = user;
+    next();
+  })
 }
 
 module.exports = authenticateToken;
